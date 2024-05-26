c5dc2b23a0d0c2eae9a018b90339139a77bf05dd
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-05-26 22:13:19 +0200

    font-display: swap;

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Notfound() {
-  return (
-<main className='min-h-screen'>
-  
-
-  <div className="flex-col items-center justify-center text-center">
-    <h1 className="text-9xl font-bold text-white">404</h1>
-    <p className="text-2xl mt-4 text-gray-300">Vous Vous êtes perdu</p>
-  </div>
-
-  </main>
-  )
+	return (
+		<main className="min-h-screen">
+			<div className="flex-col items-center justify-center text-center">
+				<h1 className="text-9xl font-bold text-white">404</h1>
+				<p className="text-2xl mt-4 text-gray-300">Vous Vous êtes perdu</p>
+			</div>
+		</main>
+	);
 }
