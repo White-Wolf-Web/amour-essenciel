f46f629b912c36bf7f291a92866753c996c57347
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-06-03 12:46:27 +0200

    Update footer.tsx

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,13 +2,19 @@ import Link from "next/link";
 
 export default function Footer() {
 	return (
-		<footer className='text-xs  flex justify-around py-2 mt-5 rounded-t'>
-			<Link href="https://www.creation-site-internet.dev/">
-				<p>CSI</p></Link>
-			<p className='hidden xxs:block'>MG</p>
-			<p>0497.33.09.44</p>
-			<p>amouressenciel@gmail.com</p>
-			<p className='hidden sm:block'>©AmourEssenciel 2024</p> 
+		<footer className="text-xs  flex-col  justify-around md:flex py-2 mt-5 rounded-t">
+			<div className="flex justify-around">
+				<Link href="https://www.creation-site-internet.dev/">
+					<p>Créé par CSI</p>
+				</Link>
+				<p>Design par MG</p>
+			</div>
+
+			<div className="flex justify-around">
+				<p>0497.33.09.44</p>
+				<p>amouressenciel@gmail.com</p>
+				<p className="hidden sm:block">©AmourEssenciel 2024</p>{" "}
+			</div>
 		</footer>
 	);
 }
